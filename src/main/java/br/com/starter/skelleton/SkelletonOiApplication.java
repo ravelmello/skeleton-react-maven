7dd7bf6ba92ff2dc0a574ce21ffbdfac99f5343c
ravelmello <ravelmello3@gmail.com>
2019-12-15 15:48:40 -0300

    Add src folder

---
+++ src/main/java/br/com/starter/skelleton/SkelletonOiApplication.java
@@ -0,0 +1,15 @@
+package br.com.starter.skelleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkelletonOiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkelletonOiApplication.class, args);
+		System.out.println("Iniciando Aplicação");
+	}
+
+
+}
